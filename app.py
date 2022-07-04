8aa6986bdce3bf81d73b4deccef3f7dcaa1801d1
devisap <ilham.sagitaputra@gmail.com>
2022-07-05 00:06:30 +0700

    scrap 2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+import time
 from flask import Flask, jsonify, json
 from scrapper import get_data_hashtag, get_data_post
 from datetime import datetime
@@ -21,22 +23,54 @@ def index():
     # post['COUNTLIKE_DD']    = post_scrape['like_count']
     # post['COUNTCOMMENT_DD'] = post_scrape['comment_count']
     # post['CAPTION_DD']      = post_scrape['']
+    i = 0
+    x = 1
     for hashtag_scrape in hashtag_scrapes:
-        hashtag_scrape = hashtag_scrape['node']
+        try:
+            # if i == 4:
+            #     i = 0
+            #     time.sleep(3)
+            #     if x == 20:
+            #         x = 1
+            #         time.sleep(5)
 
-        post_scrape = get_data_post(hashtag_scrape['shortcode'])
-        post_scrape = post_scrape['items'][0]
+            hashtag_scrape = hashtag_scrape['node']
 
-        post = {}
-        post['SHORTCODE_DD']    = hashtag_scrape['shortcode']
-        post['USERNAME_DD']     = post_scrape['user']['username']
-        post['DISPLAYURL_DD']   = post_scrape['image_versions2']['candidates'][0]['url']
-        post['COUNTLIKE_DD']    = post_scrape['like_count']
-        post['COUNTCOMMENT_DD'] = post_scrape['comment_count']
-        post['CAPTION_DD']      = post_scrape['caption']['text']
-        json.dumps(post)
+            post_scrape = get_data_post(hashtag_scrape['shortcode'])
+            post_scrape = post_scrape['items'][0]
 
-        list_data.append(post)
+            post = {}
+            post['SHORTCODE_DD']    = hashtag_scrape['shortcode']
+            post['USERNAME_DD']     = post_scrape['user']['username']
+            post['FULLNAME_DD']     = post_scrape['user']['full_name']
+            post['PROFILPICT_DD']     = post_scrape['user']['profile_pic_url']
+            # post['DISPLAYURL_DD']   = post_scrape['image_versions2']['candidates'][0]['url']
+            post['DISPLAYURL_DD']   = hashtag_scrape['display_url']
+            post['COUNTLIKE_DD']    = post_scrape['like_count']
+            post['COUNTCOMMENT_DD'] = post_scrape['comment_count']
+            post['CAPTION_DD']      = post_scrape['caption']['text']
+
+            try:
+                list_tags = post_scrape['usertags']['in']
+                tag_users = []
+                
+                for tag in list_tags:
+                    tag_users.append(tag['user']['username'])
+                post['LISTTAG_DD'] = ';'.join(tag_users)
+            except:
+                post['LISTTAG_DD']      = None
+
+            json.dumps(post)
+            list_data.append(post)
+
+            print(x," | ",hashtag_scrape['shortcode'])
+            time.sleep(1)
+
+            
+            i = i + 1
+            x = x + 1
+        except: 
+            return jsonify(list_data)
 
     # return jsonify(hashtag_scrapes[0]['node']['shortcode'])
     return jsonify(list_data)
--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,7 @@ import browser_cookie3
 import time, json
 
 def get_data_hashtag (hashtag):
-    cookiejar = browser_cookie3.firefox(domain_name='instagram.com')
+    cookiejar = browser_cookie3.chrome(domain_name='instagram.com')
     # scrape = requests.get(f'https://www.instagram.com/explore/tags/{hashtag}/?__a=1&__d=dis', cookies=cookiejar).json()
     scrape = requests.get(f'https://www.instagram.com/explore/tags/{hashtag}/?__a=1&__d=dis', cookies=cookiejar).json()
     return scrape
